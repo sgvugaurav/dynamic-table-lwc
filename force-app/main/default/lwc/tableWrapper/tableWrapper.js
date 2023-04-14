ff17004887bb3836c75fb756294160a309a09f66
Gaurav Kumar <gauav9142@hotmail.com>
2023-04-14 12:43:36 +0530

    implemented save opration

--- force-app/main/default/lwc/tableWrapper/tableWrapper.js
+++ force-app/main/default/lwc/tableWrapper/tableWrapper.js
@@ -1,5 +1,6 @@
-import getData from '@salesforce/apex/DynamicTableController.getData';
 import { LightningElement, api, track } from 'lwc';
+import getData from '@salesforce/apex/DynamicTableController.getData';
+import saveData from '@salesforce/apex/DynamicTableController.saveData';
 import LightningAlert from 'lightning/alert';
 import LightningConfirm from 'lightning/confirm';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
@@ -77,14 +78,22 @@ export default class TableWrapper extends LightningElement {
             if (result) {
                 if (id.startsWith('local')) {
                     this.removeRecordWithId(id);
-                    this.recordMap.delete(id);
+                    if (this.recordMap.has(id)) {
+                        const record = this.recordMap.get(id);
+                        this.recordChangeSet.delete(record);
+                        this.recordMap.delete(id);
+                    }
                 } else {
                     this.isLoading = true;
                     deleteRecord(id)
                     .then(res => {
                         console.log(res);
                         this.removeRecordWithId(id);
-                        this.recordMap.delete(id);
+                        if (this.recordMap.has(id)) {
+                            const record = this.recordMap.get(id);
+                            this.recordChangeSet.delete(record);
+                            this.recordMap.delete(id);
+                        }
                         this.dispatchEvent(
                             new ShowToastEvent({
                                 title: 'Success',
@@ -121,24 +130,112 @@ export default class TableWrapper extends LightningElement {
         this.fieldList.forEach(field => {
             newRecord[field] = null;
         });
-        this.records.push(newRecord);
+        newRecord.isToday = false;
+        newRecord.styleClasses = 'slds-hint-parent';
+        // this.records.push(newRecord);
+        this.records = [...this.records, newRecord];
         this.recordMap.set(newRecord.Id, newRecord);
+        this.recordChangeSet.add(newRecord);
         console.log(newRecord);
     }
 
     handelEditAndSave(event) {
-        if (this.mode != 'edit') {
+        const label = event.target.label;
+        if (label === 'Edit' && this.mode != 'edit') {
             this.mode = 'edit';
         }
         else {
-            this.mode = 'view';
-            this.saveDisabled = true;
+            const recordList = [...this.recordChangeSet];
+            recordList.forEach(record => {
+                if (String(record.Id).startsWith('local')) {
+                    delete record.Id;
+                }
+                delete record.isToday;
+                delete record.styleClasses;
+            });
+            console.log(recordList);
+            const fieldTypes = {};
+            this.fields.forEach(f => {
+                fieldTypes[f.fieldName] = f.type;
+            })
+            this.isLoading = true;
+            saveData({
+                objectName: this.objectName,
+                records: recordList,
+                fieldTypes: fieldTypes
+            })
+            .then(data => {
+                console.log('New Data', data);
+                const newRecordList = [];
+                this.records.forEach(r => {
+                    if (r.Id != undefined && r.Id != null && !String(r.Id).startsWith('local')) {
+                        newRecordList.push(r);
+                    }
+                });
+                data.forEach(d => {
+                    newRecordList.push(d);
+                });
+                this.recordMap.clear();
+                this.recordChangeSet.clear();
+                this.records = [];
+                this.records = newRecordList;
+                this.checkDate();
+                this.records.forEach(r => {
+                    this.recordMap.set(r.Id, r);
+                });
+                this.mode = 'view';
+                this.saveDisabled = true;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Record(s) Saved',
+                        variant: 'success'
+                    })
+                );
+                console.log(this.records);
+            })
+            .catch(error => {
+                console.log('Error saving the record(s) ', error);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error saving record(s)',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+                console.log(this.records);
+            })
+            .finally(() => {
+                this.isLoading = false;
+                console.log(this.records);
+            });
         }
     }
 
     handleCellValueChange(event) {
         console.log('Id: ' + event.currentTarget.dataset.id + ' field: ' + event.currentTarget.dataset.field);
         console.log(event.detail.value);
+        const value = event.detail.value;
+        const Id = event.currentTarget.dataset.id;
+        const field = event.currentTarget.dataset.field;
+        const type = event.currentTarget.dataset.type;
+        const record = this.recordMap.get(Id);
+        record[field] = value;
+        if(this.dateFieldInfo.hasDateField && (type === 'date' || type === 'datetime')) {
+            const today = new Date();
+            const date = new Date(value);
+            if (today.getDate() === date.getDate() && today.getMonth() === date.getMonth() && today.getFullYear() === date.getFullYear()) {
+                record.isToday = true;
+                record.styleClasses = 'slds-hint-parent green-background';
+            } else {
+                record.isToday = false;
+                record.styleClasses = 'slds-hint-parent';
+            }
+        }
+        if (!this.recordChangeSet.has(record)) {
+            this.recordChangeSet.add(record);
+        }
+        console.log(this.recordChangeSet);
     }
 
     checkDate() {
